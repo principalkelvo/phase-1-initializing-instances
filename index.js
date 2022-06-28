ed3d85375be17b053dea406fa39dad27a4fb0f74
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-06-28 17:00:18 +0300

    commit tree 6d976cb493afac3abcb02efd49d4811d749f4af1

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your code here
+class Breakfast{
+    constructor(food, drink){
+        this.food = food;
+        this.drink = drink;
+    }
+}
+
+class Lunch{
+    constructor(salad, soup, drink){
+        this.salad = salad;
+        this.soup = soup;
+        this.drink = drink;
+    }
+}
+
+class Dinner{
+    #desert;
+    constructor(salad, soup, entree, desert){
+        this.salad = salad;
+        this.soup = soup;
+        this.entree = entree;
+        this.#desert = desert;
+    }
+}
